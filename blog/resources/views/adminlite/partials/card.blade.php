d0440a2f93148551da6fc6ddf3aaf66496e6cee4
sendyjoan <sendyjoan5@gmail.com>
2020-08-23 00:35:18 +0700

    menambahkan card baru dalam home page admin

--- blog/resources/views/adminlite/partials/card.blade.php
+++ blog/resources/views/adminlite/partials/card.blade.php
@@ -22,5 +22,26 @@
   <!-- /.card-footer-->
 </div>
 <!-- /.card -->
+<div class="card">
+  <div class="card-header">
+    <h3 class="card-title">Admin 1</h3>
+
+    <div class="card-tools">
+      <button type="button" class="btn btn-tool" data-card-widget="collapse" data-toggle="tooltip" title="Collapse">
+        <i class="fas fa-minus"></i></button>
+      <button type="button" class="btn btn-tool" data-card-widget="remove" data-toggle="tooltip" title="Remove">
+        <i class="fas fa-times"></i></button>
+    </div>
+  </div>
+  <div class="card-body">
+    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi nemo labore reiciendis dicta mollitia eos. Dignissimos, cum quisquam. Praesentium, nisi eveniet eum magni esse accusamus minus nesciunt hic vitae eaque.
+  </div>
+  <!-- /.card-body -->
+  <div class="card-footer">
+    ~Sendy
+  </div>
+  <!-- /.card-footer-->
+</div>
+<!-- /.card -->
 
 </section>
