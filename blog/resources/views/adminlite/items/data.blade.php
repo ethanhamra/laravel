c603745ea3f4a5234e961ade505fc4b81a754aa1
sendyjoan <sendyjoan5@gmail.com>
2020-08-28 14:33:29 +0700

    menyempurnakan templating blade

--- blog/resources/views/adminlite/items/data.blade.php
+++ blog/resources/views/adminlite/items/data.blade.php
@@ -441,7 +441,7 @@
     </div>
 @endsection
 
-@push('script')
+@push('scripts')
 <script src=" {{asset('/adminlte/plugins/datatables/jquery.dataTables.js')}} "></script>
 <script src=" {{asset('/adminlte/plugins/datatables-bs4/js/dataTables.bootstrap4.js')}} "></script>
 <script>
--- blog/resources/views/adminlite/master.blade.php
+++ blog/resources/views/adminlite/master.blade.php
@@ -53,7 +53,7 @@
   <!-- /.control-sidebar -->
 </div>
 <!-- ./wrapper -->
-@stack('script')
+
 <!-- jQuery -->
 <script src="{{asset('/adminlte/plugins/jquery/jquery.min.js')}} "></script>
 <!-- Bootstrap 4 -->
@@ -62,6 +62,6 @@
 <script src="{{asset('/adminlte/dist/js/adminlte.min.js')}} "></script>
 <!-- AdminLTE for demo purposes -->
 <script src="{{asset('/adminlte/dist/js/demo.js')}} "></script>
-
+@stack('scripts')
 </body>
 </html>
